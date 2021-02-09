62e38b7103ff7240ff16d40b5a2e6c2c38f270e1
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2021-02-09 08:47:51 +0300

    g

--- app/src/main/java/com/example/gambit/SplashActivity.java
+++ app/src/main/java/com/example/gambit/SplashActivity.java
@@ -1,10 +1,9 @@
 package com.example.gambit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,15 +12,8 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },1500);
-
-
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
